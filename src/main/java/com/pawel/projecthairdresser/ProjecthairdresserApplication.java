dea1613185fa066c9e93416911a4a88a93775576
unknown <pawelek1227@gmail.com>
2018-10-12 23:03:33 +0200

    Starting project

---
+++ src/main/java/com/pawel/projecthairdresser/ProjecthairdresserApplication.java
@@ -0,0 +1,12 @@
+package com.pawel.projecthairdresser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecthairdresserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecthairdresserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pawel/projecthairdresser/ProjecthairdresserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pawel.projecthairdresser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjecthairdresserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
